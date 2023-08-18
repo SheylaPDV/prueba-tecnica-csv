f096b63cdf106df6b81108dc14ddb5960bd7880c
Sheyla <sheylapdv@gmail.com>
2023-08-18 12:28:11 +0200

    resolution of exercises

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const fs = require("fs");
 const csv = require("csv-parser");
 
-const results = [];
-let hombres = 0;
-let mujeres = 0;
-let sumaSalarios = 0;
+const results = []; // list of employees
+
+let men = 0; // men counter
+let women = 0; // women counter
+let totalSalaries = 0; // sumatory of salaries
+
+// csv reading
 fs.createReadStream("datos_prueba_tecnica.csv")
   .pipe(
     csv({
       separator: ";",
       newline: "\n",
       mapHeaders: ({ header, index }) =>
+        // Normalize headers
         header
           .toLowerCase()
           .trim()
@@ -19,23 +23,41 @@ fs.createReadStream("datos_prueba_tecnica.csv")
     })
   )
   .on("data", (data) => results.push(data))
+
   .on("end", () => {
     results.forEach((res, index) => {
-      if (res.sexo == "H") {
-        hombres++;
-      } else if (res.sexo == "M") {
-        mujeres++;
+      // genre checking
+      if (res.genre == "H") {
+        men++;
+      } else if (res.genre == "M") {
+        women++;
       }
-      let salarios = Number(res.salario_bruto_anual);
-      sumaSalarios += salarios;
+
+      // Sumatory of salaries
+      totalSalaries += Number(res.salario_bruto_anual);
+
+      // Checking Equilibrha HR employees with salary above 28000
+      if (
+        res.salario_bruto_anual > "28000" &&
+        res.nombre_empresa == "Equilibrha RRHH"
+      ) {
+        console.log(
+          res.id_empleado,
+          res.nombre,
+          res.apellido1,
+          res.apellido2,
+          res.salario_bruto_anual,
+          res.nombre_empresa
+        );
+      }
     });
 
     console.log(
       `En total hay ${
-        hombres + mujeres
-      } empleados: ${hombres} hombres y ${mujeres} mujeres `
+        men + women
+      } empleados: ${men} hombres y ${women} mujeres `
     );
     console.log(
-      `El salario bruto anual de los empleados en la empresa es de ${sumaSalarios}`
+      `El salario bruto anual de los empleados en la empresa es de ${totalSalaries}`
     );
   });
